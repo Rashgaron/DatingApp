bacbbefc30138c5976ee92e806c07694036fa98c
rashgaron <daniel.rodiguez.zamorano@est.fib.upc.edu>
2021-06-03 08:44:12 -0400

    Deleting index

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+//Decorator, way of giving a class extra power
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'client';
+  title = 'The dating app';
 }
