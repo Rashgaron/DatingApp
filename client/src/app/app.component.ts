ca3d1b0bca9b44319ef4194085403249ad57ef30
rashgaron <daniel.rodiguez.zamorano@est.fib.upc.edu>
2021-06-03 09:03:07 -0400

    Added fetch to api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
+const apiUrl = 'https://localhost:5001/api/users';
 //Decorator, way of giving a class extra power
 
 @Component({
@@ -8,6 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'The dating app';
+  users:any; // Uses any for any type of return. It can be anything. Whatever it returns
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(){
+    this.getUsers();
+  }
+
+
+
+
+  //funcion to get all users 
+  getUsers(){
+    this.http.get(apiUrl)
+      .subscribe(response => {
+        this.users = response;
+      }, error => {
+        console.log(error);
+      });
+  }
+  
 }
