69eeb6046645d6ed944a7023aae73abd2e0af3d4
rashgaron <daniel.rodiguez.zamorano@est.fib.upc.edu>
2021-06-22 18:35:18 -0400

    Adding seed

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -11,7 +11,6 @@ namespace Entities
         public string UserName { get; set; }
         
         public byte[] PasswordHash { get; set; }
-
         public byte[] PasswordSalt { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
