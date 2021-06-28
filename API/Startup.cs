0bd747f704cafbd05911024e0dbd6ca9832ee8e7
rashgaron <45242200+daniel.rodriguez.zamorano@estudiantat.upc.edu>
2021-06-28 03:15:37 +0200

    Uploading photos

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,9 @@
-using System;
-using System.Text;
-using API.Interfaces;
-using API.Services;
-using Data;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using API.Extensions;
 using API.Middleware;
 
